b6f46ab0f1d32f6ce1e79578d94b2dbefd7350d6
Zainul <zainulbasharnazmin@gmail.com>
2023-06-20 21:43:21 +0530

    second-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[data,setData] = useState([]);
+
+  useEffect(() =>{
+    fetch('https://dummyjson.com/products')
+    .then((res) => res.json())
+    .then((products) => setData(products))
+  },[])
+ 
   return (
     <div>
         {/* Do not remove the main div */}
+      <pre>{data}</pre>
     </div>
   )
 }
